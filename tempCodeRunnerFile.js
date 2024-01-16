be756766145fba4ac2951146309d7645c92229a4
Misha <mzuew2006@gmail.com>
2024-01-16 22:17:29 +0200

    end of cycles

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const arr = [3, 5, 8, 16, 20, 23, 50];
+    const resullt = [];
+
+    // Пишем решение вот тут
+    
+    for (let i = 0; i < arr.length; i++) {
+        resullt[i] = arr[i];
+    }
+    console.log(resullt)
